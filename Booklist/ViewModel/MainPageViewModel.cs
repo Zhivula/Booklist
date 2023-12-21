b5549d1cb2fa8e37c3a51c1fa20226e52609733c
Zhivula <56290740+Zhivula@users.noreply.github.com>
2023-12-21 23:09:02 +0300

    -

--- Booklist/ViewModel/MainPageViewModel.cs
+++ Booklist/ViewModel/MainPageViewModel.cs
@@ -4,6 +4,7 @@ using Booklist.Model;
 using Booklist.Resourses;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -15,8 +16,14 @@ namespace Booklist.ViewModel
 {
     class MainPageViewModel : INotifyPropertyChanged
     {
-        private List<BookFilter> books;
-        public List<BookFilter> Books
+        private bool flagBookTitles;
+        private bool flagAuthors;
+        private bool flagMarks;
+        private bool flagPages;
+        private bool flagDate;
+
+        private ObservableCollection<BookFilter> books;
+        public ObservableCollection<BookFilter> Books
         {
             get => books;
             set
@@ -29,9 +36,9 @@ namespace Booklist.ViewModel
         {
             Books = GetListBookFilter();
         }
-        private List<BookFilter> GetListBookFilter()
+        private ObservableCollection<BookFilter> GetListBookFilter()
         {
-            var filterList = new List<BookFilter>();
+            var filterList = new ObservableCollection<BookFilter>();
             foreach (var i in new MainPageModel().ListMainPage)
             {
                 filterList.Add(new BookFilter() {
@@ -46,6 +53,72 @@ namespace Booklist.ViewModel
             }
             return filterList;
         }
+        
+        public ICommand SortBookTitles => new DelegateCommand(o =>
+        {
+            if (flagBookTitles)
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderByDescending(x => x.Name));
+                flagBookTitles = false;
+            }
+            else
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderBy(x => x.Name));
+                flagBookTitles = true;
+            }
+        });
+        public ICommand SortAuthors => new DelegateCommand(o =>
+        {
+            if (flagAuthors)
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderByDescending(x => x.Author));
+                flagAuthors = false;
+            }
+            else
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderBy(x => x.Author));
+                flagAuthors = true;
+            }
+        });
+        public ICommand SortMarks => new DelegateCommand(o =>
+        {
+            if (flagMarks)
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderByDescending(x => x.Mark));
+                flagMarks = false;
+            }
+            else
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderBy(x => x.Mark));
+                flagMarks = true;
+            }
+        });
+        public ICommand SortDate => new DelegateCommand(o =>
+        {
+            if (flagDate)
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderByDescending(x => x.Date));
+                flagDate = false;
+            }
+            else
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderBy(x => x.Date));
+                flagDate = true;
+            }
+        });
+        public ICommand SortPages => new DelegateCommand(o =>
+        {
+            if (flagPages)
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderByDescending(x => x.Pages));
+                flagPages = false;
+            }
+            else
+            {
+                Books = new ObservableCollection<BookFilter>(Books.OrderBy(x => x.Pages));
+                flagPages = true;
+            }
+        });
         #region PropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string name)
